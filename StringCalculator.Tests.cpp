13eacfcb58b3ee6cdd1ffdd082fab86d0322c8a3
Anupriya Manuja <Anupriya.Manuja@in.bosch.com>
2024-08-20 15:21:15 +0530

    StringCalculator first commit 3

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -25,3 +25,68 @@ TEST(string_calculator_add_when_passed_a_single_number,returns_1_for_1){
   int actualSum=calculator.Add(input);
   ASSERT_EQ(actualSum,expectedsum);
 }
+
+TEST(string_calculator_add_when_passed_two_comma_delimited_numbers, returns_their_sum){
+  StringCalculator calculator;
+  string input="1,2";
+  int expectedsum=3;
+  int actualSum=calculator.Add(input);
+  ASSERT_EQ(actualSum,expectedsum);
+}
+
+TEST(string_calculator_add_when_passed_multiple_comma_delimited_numbers, returns_their_sum){
+  StringCalculator calculator;
+  string input="1,2,3";
+  int expectedsum=6;
+  int actualSum=calculator.Add(input);
+  ASSERT_EQ(actualSum,expectedsum);
+}
+
+TEST(string_calculator_add_when_delimited_with_newline_and_comma, returns_their_sum){
+  StringCalculator calculator;
+  string input="1\n2,3";
+  int expectedsum=6;
+  int actualSum=calculator.Add(input);
+  ASSERT_EQ(actualSum,expectedsum);
+}
+
+TEST(string_calculator_add_when_passed_a_delimiter, returns_their_sum){
+  StringCalculator calculator;
+  string input="//;\n1;2";
+  int expectedsum=3;
+  int actualSum=calculator.Add(input);
+  ASSERT_EQ(actualSum,expectedsum);
+}
+
+
+TEST(string_calculator_add_when_passed_numbers_over_1000, ignores_them){
+  StringCalculator calculator;
+  string input="42,1001,3";
+  int expectedsum=45;
+  int actualSum=calculator.Add(input);
+  ASSERT_EQ(actualSum,expectedsum);
+}
+
+TEST(string_calculator_add_when_passed_multicharacter_delimiter,uses_that_delimiter_to_sum_values){
+  StringCalculator calculator;
+  string input="//[***]\n8***2***3";
+  int expectedsum=13;
+  int actualSum=calculator.Add(input);
+  ASSERT_EQ(actualSum,expectedsum);
+}
+
+TEST(string_calculator_add_when_passed_multiple_delimiters,sums_on_each_delimiter){
+  StringCalculator calculator;
+  string input="//[*][%]\n4*2%3";
+  int expectedsum=9;
+  int actualSum=calculator.Add(input);
+  ASSERT_EQ(actualSum,expectedsum);
+}
+
+TEST(string_calculator_add_when_passed_multiple_multicharacter_delimiters,sums_on_each_delimiter){
+  StringCalculator calculator;
+  string input="//[**][%^]\n4**1%^9";
+  int expectedsum=14;
+  int actualSum=calculator.Add(input);
+  ASSERT_EQ(actualSum,expectedsum);
+}
