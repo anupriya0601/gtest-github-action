b1f792495f29f4dd7e261f20a92ce22fc4b41a16
Anupriya Manuja <Anupriya.Manuja@in.bosch.com>
2024-08-20 15:08:49 +0530

    StringCalculator first commit 2

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -11,3 +11,17 @@ TEST(string_calculator_add_when_passed_a_single_number,returns_0_for_empty_strin
   //Assert
   ASSERT_EQ(actualSum,expectedsum);
 }
+TEST(string_calculator_add_when_passed_a_single_number,returns_0_for_zero){
+  StringCalculator calculator;
+  string input="0";
+  int expectedsum=0;
+  int actualSum=calculator.Add(input);
+  ASSERT_EQ(actualSum,expectedsum);
+}
+TEST(string_calculator_add_when_passed_a_single_number,returns_1_for_1){
+  StringCalculator calculator;
+  string input="1";
+  int expectedsum=1;
+  int actualSum=calculator.Add(input);
+  ASSERT_EQ(actualSum,expectedsum);
+}
