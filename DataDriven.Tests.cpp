b44e30cf7423f30a38d3730dd084cc9a9271844d
Anupriya Manuja <Anupriya.Manuja@in.bosch.com>
2024-08-21 11:01:43 +0530

    Parameterized tests4

--- DataDriven.Tests.cpp
+++ DataDriven.Tests.cpp
@@ -15,10 +15,20 @@ class StringCalculatorParamFixture: public StringCalculatorAddTestFixture,
                                     
 };
 
-INSTANTIATE_TEST_SUITE_P(SingleNumerParameter, StringCalculatorParamFixture,
+INSTANTIATE_TEST_SUITE_P(StringCalculatorTestSuite, StringCalculatorParamFixture,
                          ::testing::Values(
+                             std::make_tuple("", 0),
+                             std::make_tuple("0", 0),
                              std::make_tuple("1", 1),
-                             std::make_tuple("1,2",3)
+                             std::make_tuple("1,2",3),
+                             std::make_tuple("1,2,3",6),
+                             std::make_tuple("1\n2,3",6),
+                             std::make_tuple("//;\n1;2",3),
+                             std::make_tuple("42,1001,3",45),
+                             std::make_tuple("//[***]\n8***2***3",13),
+                             std::make_tuple("//[*][%]\n4*2%3",9),
+                             std::make_tuple("//[**][%^]\n4**1%^9",14)
+
 ));
 
 TEST_P(StringCalculatorParamFixture, CheckCalculatorResult) {
@@ -28,4 +38,9 @@ TEST_P(StringCalculatorParamFixture, CheckCalculatorResult) {
   int actualSum=calculator.Add(input);
   //Assert
   ASSERT_EQ(actualSum,expectedsum);
+}
+
+TEST_F(StringCalculatorAddTestFixture, string_calculator_add_when_passed_negative_number_throws_an_exception_listing_invalid_values){
+  string input="1,-2,-4,5";
+  ASSERT_THROW(calculator.Add(input),invalid_argument);
 }
